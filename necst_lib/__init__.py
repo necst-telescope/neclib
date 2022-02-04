ad0f7b66d104e526f6dd5ffc20c5246319b33654
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-02-04 18:11:08 +0900

    #1 Fix version definition

--- necst_lib/__init__.py
+++ necst_lib/__init__.py
@@ -1,2 +1,11 @@
-__version__ = "0.1.0"
-__author__ = "Kaoru Nishikawa"
+# flake8: noqa
+
+try:
+    from importlib_metadata import version
+except ImportError:
+    from importlib.metadata import version  # Python 3.8+
+
+try:
+    __version__ = version("necst_lib")
+except:
+    __version__ = "0.0.0"
