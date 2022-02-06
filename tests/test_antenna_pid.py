8f10e638f43b92f92e479ecf37c91c8581ea6ccb
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-02-06 11:02:23 +0900

    #3 Add a file for antenna_pid test

---
+++ tests/test_antenna_pid.py
@@ -0,0 +1,15 @@
+from necst_lib.antenna_pid import PIDController
+
+
+class TestPIDController:
+    def test_io_unit(self):
+        pass
+
+    def test_speed_limit(self):
+        pass
+
+    def test_acceleration_limit(self):
+        pass
+
+    def test_suitable_angle(self):
+        pass
