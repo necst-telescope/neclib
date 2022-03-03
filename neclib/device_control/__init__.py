de4efee2c90c2caccee969d5251ed05681198810
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-03-03 23:42:03 +0900

    #11 Modify docstring

--- neclib/device_control/__init__.py
+++ neclib/device_control/__init__.py
@@ -1,3 +1,10 @@
 # flake8: noqa
 
+"""Controllers for miscellaneous devices.
+
+Controller may refer to collection of functions to control devices or implementation of
+some algorithms to control something.
+
+"""
+
 from .antenna_pid import *
