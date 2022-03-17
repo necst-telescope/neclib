558283a61008f8797a156529069ade35117128cd
Takeru Nishioka <tnishioka@a.phys.nagoya-u.ac.jp>
2022-03-17 15:33:16 +0900

    #4 Write code of pointing error correction

--- neclib/pointing_error/correction.py
+++ neclib/pointing_error/correction.py
@@ -1,78 +1,54 @@
+from n_const import PointingError
 import numpy as np
-from astropy.coordinates import SkyCoord
-import xarray as xr
+
+from .. import utils
 
 
-def apply_kisa_test(azel, hosei):
+def derive_pointing_error(az, el, pointing_params_file):
+    az, el = utils.force_data_type(az, el)
 
-    if isinstance(azel, SkyCoord):
-
-        az = azel.az.rad
-        el = azel.alt.rad
-
-    elif isinstance(azel, tuple):
-
-        if isinstance(azel[0], np.ndarray):
-            az = np.deg2rad(azel[0])
-            el = np.deg2rad(azel[1])
-
-        if isinstance(azel[0], xr.DataArray):
-            az = np.deg2rad(azel[0].values)
-            el = np.deg2rad(azel[1].values)
-
-    else:
-        raise (TypeError("Input must be a Skycoord or a tuple of ndarray or xarray"))
-
-    with open(hosei, "r") as f:
-        kisa = f.read().splitlines()
-    kisa = [float(n) for n in kisa]
-
-    kisa[3] = np.deg2rad(kisa[3])
-    kisa[6] = np.deg2rad(kisa[6])
-    kisa[8] = np.deg2rad(kisa[8])
-    kisa[10] = np.deg2rad(kisa[10])
-    kisa[19] = np.deg2rad(kisa[19])
-    el_d = np.rad2deg(el)
+    params = PointingError(pointing_params_file)
 
     dx = (
-        kisa[2] * np.sin(kisa[3] - az) * np.sin(el)
-        + kisa[4] * np.sin(el)
-        + kisa[0] * np.cos(el)
-        + kisa[1]
-        + kisa[5] * np.cos(2 * (kisa[6] - az)) * np.sin(el)
-        + kisa[16]
-        + kisa[18] * np.cos(el + kisa[19])
+        chi_Az * np.sin(omega_Az - az) * np.sin(el)
+        + eps * np.sin(el)
+        + chi2_Az * np.sin(2 * (omega2_Az - az)) * np.sin(el)
+        + dAz * np.cos(el)
+        + de
+        + cor_v * np.cos(el + cor_p)
+        + de_radio
     ) / np.cos(el)
 
     dy = (
-        -kisa[7] * np.cos(kisa[8] - az)
-        - kisa[9] * np.sin(2 * (kisa[10] - az))
-        + kisa[15]
-        + kisa[11] * el_d
-        + kisa[12] * el_d ** 2
-        + kisa[13] * el_d ** 3
-        + kisa[14] * el_d ** 4
-        + kisa[17]
-        - kisa[18] * np.sin(el + kisa[19])
-        + kisa[20] * el_d
-        + kisa[21] * el_d ** 2
-        + kisa[22] * el_d ** 3
-        + kisa[23] * el_d ** 4
+        -chi_El * np.cos(omega_El - az)
+        - chi2_El * np.cos(2 * (omega2_El - az))
+        + g * el
+        + gg * el ** 2
+        + ggg * el ** 3
+        + gggg * el ** 4
+        + dEl
+        + g_radio * el
+        + gg_radio * el ** 2
+        + ggg_radio * el ** 3
+        + gggg_radio * el ** 4
+        - cor_v * np.sin(el + cor_p)
+        + dEl_radio
     )
 
-    if isinstance(azel, SkyCoord):
+    return dx, dy
 
-        paz = azel.az.deg + dx
-        pel = azel.alt.deg + dy
 
-    elif isinstance(azel, tuple):
+def cor2ref(az, el, pointing_params_file):
+    dx, dy = derive_pointing_error(az, el, pointing_params_file)
+    refracted_az = az + dx
+    refracted_el = el + dy
 
-        if isinstance(azel[0], np.ndarray):
-            paz = azel[0] + dx
-            pel = azel[1] + dy
+    return refracted_az, refracted_el
 
-        if isinstance(azel[0], xr.DataArray):
-            paz = azel[0].values + dx
-            pel = azel[1].values + dy
-            
-    return paz, pel
+
+def ref2cor(az, el, pointing_params_file):
+    dx, dy = derive_pointing_error(az, el, pointing_params_file)
+    corrected_az = az - dx
+    corrected_el = el - dy
+
+    return corrected_az, corrected_el
