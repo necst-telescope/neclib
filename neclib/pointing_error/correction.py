89446df6bac2316812cedf7d360714a4348d2413
Takeru Nishioka <tnishioka@a.phys.nagoya-u.ac.jp>
2022-04-06 18:24:16 +0900

    #4 Debug code of pointing error correction

--- neclib/pointing_error/correction.py
+++ neclib/pointing_error/correction.py
@@ -7,48 +7,48 @@ from .. import utils
 def derive_pointing_error(az, el, pointing_params_file):
     az, el = utils.force_data_type(az, el)
 
-    params = PointingError(pointing_params_file)
+    p = PointingError(pointing_params_file)
 
-    dx = (
-        chi_Az * np.sin(omega_Az - az) * np.sin(el)
-        + eps * np.sin(el)
-        + chi2_Az * np.sin(2 * (omega2_Az - az)) * np.sin(el)
-        + dAz * np.cos(el)
-        + de
-        + cor_v * np.cos(el + cor_p)
-        + de_radio
+    dAz = (
+        p.chi_Az * np.sin(p.omega_Az - az) * np.sin(el)
+        + p.eps * np.sin(el)
+        + p.chi2_Az * np.sin(2 * (p.omega2_Az - az)) * np.sin(el)
+        + p.dAz * np.cos(el)
+        + p.de
+        + p.cor_v * np.cos(el + p.cor_p)
+        + p.de_radio
     ) / np.cos(el)
 
-    dy = (
-        -chi_El * np.cos(omega_El - az)
-        - chi2_El * np.cos(2 * (omega2_El - az))
-        + g * el
-        + gg * el ** 2
-        + ggg * el ** 3
-        + gggg * el ** 4
-        + dEl
-        + g_radio * el
-        + gg_radio * el ** 2
-        + ggg_radio * el ** 3
-        + gggg_radio * el ** 4
-        - cor_v * np.sin(el + cor_p)
-        + dEl_radio
+    dEl = (
+        -p.chi_El * np.cos(p.omega_El - az)
+        - p.chi2_El * np.cos(2 * (p.omega2_El - az))
+        + p.g * el
+        + p.gg * el ** 2
+        + p.ggg * el ** 3
+        + p.gggg * el ** 4
+        + p.dEl
+        + p.g_radio * el
+        + p.gg_radio * el ** 2
+        + p.ggg_radio * el ** 3
+        + p.gggg_radio * el ** 4
+        - p.cor_v * np.sin(el + p.cor_p)
+        + p.dEl_radio
     )
 
-    return dx, dy
+    return dAz, dEl
 
 
-def cor2ref(az, el, pointing_params_file):
-    dx, dy = derive_pointing_error(az, el, pointing_params_file)
-    refracted_az = az + dx
-    refracted_el = el + dy
+def enc2ref(az, el, pointing_params_file):
+    dAz, dEl = derive_pointing_error(az, el, pointing_params_file)
+    refracted_az = az + dAz
+    refracted_el = el + dEl
 
     return refracted_az, refracted_el
 
 
-def ref2cor(az, el, pointing_params_file):
-    dx, dy = derive_pointing_error(az, el, pointing_params_file)
-    corrected_az = az - dx
-    corrected_el = el - dy
+def ref2enc(az, el, pointing_params_file):
+    dAz, dEl = derive_pointing_error(az, el, pointing_params_file)
+    encoder_az = az - dAz
+    encoder_el = el - dEl
 
-    return corrected_az, corrected_el
+    return encoder_az, encoder_el
