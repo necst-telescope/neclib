a80b89f8d416c8b7cce99e65b85491b30889faea
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-04-12 19:51:40 +0900

    #42 Skip documentation tests for older Python versions

--- tests/docs/test_build.py
+++ tests/docs/test_build.py
@@ -1,9 +1,12 @@
+import sys
 import subprocess
 from pathlib import Path
 
 import pytest
 
+
 project_root = Path(__file__).parent.parent.parent
+python_version = tuple(sys.version_info)[:2]
 
 
 @pytest.fixture(scope="module")
@@ -11,6 +14,10 @@ def tmp_project_dir(tmp_path_factory) -> Path:
     return tmp_path_factory.mktemp("neclib")
 
 
+@pytest.mark.skipif(
+    python_version < (3, 9),
+    reason="No need to support that wide versions for documentation building",
+)
 def test_create_stub(tmp_project_dir: Path):
     _ = subprocess.run(
         ["cp", "-rv", ".", str(tmp_project_dir)],
@@ -36,6 +43,10 @@ def test_create_stub(tmp_project_dir: Path):
     assert result.stderr == b""
 
 
+@pytest.mark.skipif(
+    python_version < (3, 9),
+    reason="No need to support that wide versions for documentation building",
+)
 def test_build(tmp_project_dir: Path):
     assert (tmp_project_dir / "docs" / "conf.py").exists()
     result = subprocess.run(
