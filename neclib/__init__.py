a54c003f0d946cf49e2894f85297dc1edebd4d50
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-04-22 12:38:28 +0900

    #49 Add data format definition

--- neclib/__init__.py
+++ neclib/__init__.py
@@ -1,9 +1,9 @@
 # flake8: noqa
 
 try:
-    from importlib_metadata import version
-except ImportError:
     from importlib.metadata import version  # Python 3.8+
+except ImportError:
+    from importlib_metadata import version
 
 try:
     __version__ = version("neclib")
@@ -12,6 +12,12 @@ except:
 
 # Aliases
 from .device_control import *
+from .exceptions import *
+
+# Submodules
+from . import units
 
 # Subpackages
+from . import parameters
 from . import simulator
+from . import utils
