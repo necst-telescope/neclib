ba32a6b11ffdb28e31cba4a97e113074b96d6342
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-04-23 13:21:22 +0900

    #59 Implement observation parameter calculators

--- neclib/parameters/obsparam.py
+++ neclib/parameters/obsparam.py
@@ -1,13 +1,107 @@
-__all__ = ["interval", "off_point_coord"]
+"""Parameter converters."""
 
-from typing import Tuple, Union
+__all__ = ["ObsParams", "interval", "off_point_coord"]
+
+from typing import Any, List, Tuple, Union
 
 import astropy.units as u
 import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy.coordinates.baseframe import BaseCoordinateFrame
 
+from .parser import ObsParamData
 from .. import units as custom_u
+
+# Only those not supported by Astropy
+COORDINATES = {
+    "j2000": "fk5",
+    "b1950": "fk4",
+    "horizontal": "altaz",
+}
+
+
+def translate_coordsys(coordsys: str) -> str:
+    return COORDINATES.get(coordsys.lower(), coordsys.lower())
+
+
+class ObsParams(ObsParamData):
+    """Observation parameter handler."""
+
+    ParameterName = {
+        "off_observation_interval": "off_interval",
+        "hot_observation_interval": "load_interval",
+        "on_point_coordinate": ["LamdaOn", "BetaOn", "COORD_SYS"],
+        "offset_from_on_to_off": ["deltaLamda", "deltaBeta", "DELTA_COORD"],
+        "offset_coslat_applied": "OTADEL",
+        "absolute_off_point_coordinate": ["LamdaOff", "BetaOff", "COORD_SYS"],
+    }
+
+    def _getitem(self, param_type: str) -> Union[Any, List[Any]]:
+        keys = self.ParameterName[param_type]
+
+        if isinstance(keys, str):
+            return self.get(keys, None)
+        params = [self.get(key, None) for key in keys]
+        if not any([param is None for param in params]):
+            return params
+
+    def off_observation_interval(
+        self, *, unit: Union[str, u.Unit], points_per_scan: int = None
+    ) -> Tuple[float, str]:
+        """Parse OFF observation interval.
+
+        See Also
+        --------
+        interval : Implementation of this method.
+
+        """
+        return interval(
+            self._getitem("off_observation_interval"),
+            unit=unit,
+            points_per_scan=points_per_scan,
+        )
+
+    def hot_observation_interval(
+        self, *, unit: Union[str, u.Unit], points_per_scan: int = None
+    ) -> Tuple[float, str]:
+        """Parse HOT observation interval.
+
+        See Also
+        --------
+        interval : Implementation of this method.
+
+        """
+        return interval(
+            self._getitem("hot_observation_interval"),
+            unit=unit,
+            points_per_scan=points_per_scan,
+        )
+
+    def off_point_coord(
+        self, *, unit: Union[str, u.Unit], **kwargs
+    ) -> Tuple[float, float, str]:
+        """Parse HOT observation interval.
+
+        See Also
+        --------
+        off_point_coord : Implementation of this method.
+
+        """
+
+        def translate(coordinate):
+            if coordinate is None:
+                return
+            lon, lat, coordsys = coordinate
+            return lon, lat, translate_coordsys(coordsys)
+
+        return off_point_coord(
+            unit=unit,
+            on_point=translate(self._getitem("on_point_coordinate")),
+            offset=translate(self._getitem("offset_from_on_to_off")),
+            coslat_applied=self._getitem("offset_coslat_applied"),
+            absolute=translate(self._getitem("absolute_off_point_coordinate")),
+            **kwargs,
+        )
 
 
 def interval(
@@ -58,16 +152,16 @@ def off_point_coord(
 
     Give (``absolute`` coordinate of OFF point, output ``unit``) or (coordinate of
     ``on_point``, ``offset`` from the ON point, if the offset value has
-    ``coslat_applied``, output ``unit``). The coordinates should be given in the form:
-    (longitude, latitude, coordinate frame)
+    ``coslat_applied``, output ``unit``). The coordinates should be given in the
+    form: (longitude, latitude, coordinate frame)
 
     Parameters
     ----------
     on_point
         Coordinate of ON point.
     offset
-        Offset from ON point to OFF point. Caution the unit, as ``Angle("0h0m1s")`` is
-        not equivalent to ``Angle("1arcsec")``.
+        Offset from ON point to OFF point. Caution the unit, as ``Angle("0h0m1s")``
+        is not equivalent to ``Angle("1arcsec")``.
     coslat_applied
         If ``False``, longitude of ``offset`` will be divided by cos(latitude).
     absolute
@@ -84,7 +178,7 @@ def off_point_coord(
     ...     absolute=(Angle("3h25m20s"), Angle("31d32m33s"), "fk5"),
     ...     unit="deg",
     ... )
-    (51.3333, 31.5425, "fk5")
+    (51.3333, 31.5425, 'fk5')
     >>> off_point_coord(
     ...     on_point=(Angle("5h1m1s"), Angle("30d25m20s"), "galactic"),
     ...     offset=(Angle("0 0 10 hours"), Angle("20arcsec"), "fk5"),
@@ -94,16 +188,16 @@ def off_point_coord(
     (266.6442444454571, 48.39027455272732, 'fk5')
 
     """
-    # Indexes for tuple of length 3.
+    # Indices for tuple of length 3.
     LON, LAT, COORDSYS = 0, 1, 2
 
     def _convert_unit(
         x: u.Quantity, y: u.Quantity, coordsys: Union[str, BaseCoordinateFrame]
     ) -> Tuple[float, float, str]:
         # Extract name of ``astropy.coordinates.baseframe.BaseCoordinateFrame``.
-        # The reason ``getattr`` is used instead of ``isinstance`` or ``issubclass`` is
-        # either of the class or its instance can be given and handling them is quite
-        # complicated.
+        # The reason ``getattr`` is used instead of ``isinstance`` or ``issubclass``
+        # is either of the class or its instance can be given and handling them is
+        # quite complicated.
         coordsys = getattr(coordsys, "name", coordsys)
         return x.to_value(unit), y.to_value(unit), coordsys.lower()
 
