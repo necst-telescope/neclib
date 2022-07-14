5ff58cb99522fdffc20caa00f23160def5c45586
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-07-14 17:35:12 +0900

    #4 Move the implementation

--- neclib/parameters/pointing_error.py
+++ neclib/parameters/pointing_error.py
@@ -1,7 +1,12 @@
 """Pointing error correction."""
 
+__all__ = ["PointingError"]
+
 from typing import Iterable, Tuple, Union
+
 import astropy.units as u
+import numpy as np
+
 from .parser import PointingErrorData
 
 
@@ -26,28 +31,68 @@ class PointingError(PointingErrorData):
 
     """  # noqa: E501
 
+    def _get_offset(
+        self, az: u.Quantity, el: u.Quantity
+    ) -> Tuple[u.Quantity, u.Quantity]:
+        """Error from calculated coordinates to encoder reading."""
+        gravitational_term = np.polynomial.Polynomial(
+            [self.g, self.gg, self.ggg, self.gggg]
+        )
+        radio_gravitational_term = np.polynomial.Polynomial(
+            [self.g_radio, self.gg_radio, self.ggg_radio, self.gggg_radio]
+        )
+
+        dx = (
+            self.chi_Az * np.sin(self.omega_Az - az) * np.sin(el)
+            + self.eps * np.sin(el)
+            + self.chi2_Az * np.sin(2 * (self.omega2_Az - az)) * np.sin(el)
+            + self.dAz * np.cos(el)
+            + self.de
+            + self.cor_v * np.cos(el + self.cor_p)
+            + self.de_radio
+        )
+        dAz = dx / np.cos(el)
+        dEl = (
+            -1 * self.chi_El * np.cos(self.omega_El - az)
+            - self.chi2_El * np.cos(2 * (self.omega2_El - az))
+            + gravitational_term(el.to("deg").value) * u.deg  # TODO: Review dimension
+            + self.dEl
+            + radio_gravitational_term(el.to("deg").value)
+            * u.deg  # TODO: Review dimension
+            - self.cor_v * np.sin(el + self.cor_p)
+            + self.del_radio
+        )
+        return dAz, dEl
+
+    def _force_data_to_be_quantity(
+        self,
+        data: Union[u.Quantity, float, Iterable[float]],
+        unit: Union[str, u.Unit] = None,
+    ) -> u.Quantity:
+        if isinstance(data, u.Quantity):
+            return data if unit is None else data.to(unit)
+        return u.Quantity(data, unit=unit)
+
     def refracted2encoder(
         self,
-        az: Union[u.Quantity, Iterable[float]],
-        el: Union[u.Quantity, Iterable[float]],
+        az: Union[u.Quantity, float, Iterable[float]],
+        el: Union[u.Quantity, float, Iterable[float]],
         *,
         unit: Union[str, u.Unit] = None,
     ) -> Tuple[u.Quantity, u.Quantity]:
-        unit_is_given = hasattr(az, "unit") and hasattr(el, "unit")
-        if (unit is None) and (not unit_is_given):
-            raise ValueError("Specify az and el unit.")
-        enc_az, enc_el = az, el  # TODO: Implement the calculation.
-        return enc_az, enc_el
+        az = self._force_data_to_be_quantity(az, unit)
+        el = self._force_data_to_be_quantity(el, unit)
+        dAz, dEl = self._get_offset(az, el)
+        return az - dAz, el - dEl
 
     def encoder2refracted(
         self,
-        az: Union[u.Quantity, Iterable[float]],
-        el: Union[u.Quantity, Iterable[float]],
+        az: Union[u.Quantity, float, Iterable[float]],
+        el: Union[u.Quantity, float, Iterable[float]],
         *,
         unit: Union[str, u.Unit] = None,
     ) -> Tuple[u.Quantity, u.Quantity]:
-        unit_is_given = hasattr(az, "unit") and hasattr(el, "unit")
-        if (unit is None) and (not unit_is_given):
-            raise ValueError("Specify az and el unit.")
-        ref_az, ref_el = az, el  # TODO: Implement the calculation.
-        return ref_az, ref_el
+        az = self._force_data_to_be_quantity(az, unit)
+        el = self._force_data_to_be_quantity(el, unit)
+        dAz, dEl = self._get_offset(az, el)
+        return az + dAz, el + dEl
