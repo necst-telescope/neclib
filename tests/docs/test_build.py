9b6fc9c83632ac9f09594613fb8597082f65594e
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-03 13:07:56 +0900

    #96 Change condition to perform test suite

--- tests/docs/test_build.py
+++ tests/docs/test_build.py
@@ -6,7 +6,7 @@ import pytest
 
 
 project_root = Path(__file__).parent.parent.parent
-python_version = tuple(sys.version_info)[:2]
+python_version = sys.version_info
 
 
 @pytest.fixture(scope="module")
@@ -15,8 +15,8 @@ def tmp_project_dir(tmp_path_factory) -> Path:
 
 
 @pytest.mark.skipif(
-    python_version < (3, 9),
-    reason="No need to support that wide versions for documentation building",
+    python_version[:2] != (3, 9),
+    reason="Documentation will be built using Python 3.9",
 )
 def test_create_stub(tmp_project_dir: Path):
     _ = subprocess.run(
@@ -43,8 +43,8 @@ def test_create_stub(tmp_project_dir: Path):
 
 
 @pytest.mark.skipif(
-    python_version < (3, 9),
-    reason="No need to support that wide versions for documentation building",
+    python_version[:2] != (3, 9),
+    reason="Documentation will be built using Python 3.9",
 )
 def test_build(tmp_project_dir: Path):
     assert (tmp_project_dir / "docs" / "conf.py").exists()
