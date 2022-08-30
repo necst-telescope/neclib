842d8c37a40ad3b6d5fdff76f3bd6d837ba9f0b8
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-30 14:23:22 +0900

    #113 Define parameter parsers

--- tests/test_configation.py
+++ tests/test_configation.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 from pathlib import Path
+from types import SimpleNamespace
 from unittest.mock import patch
 
+import astropy.units as u
 import pytest
 from astropy.coordinates import EarthLocation
 
@@ -38,6 +40,15 @@ class TestConfigure:
         "location": EarthLocation(
             lon="138.472153deg", lat="35.940874deg", height="1386.0m"
         ),
+        "antenna_pid_param": SimpleNamespace(az=[1.5, 0.0, 0.0], el=[1.5, 0, 0.0]),
+        "antenna_drive_range": SimpleNamespace(
+            az=[0 << u.deg, 360 << u.deg], el=[10 << u.deg, 90 << u.deg]
+        ),
+        "antenna_drive_softlimit": SimpleNamespace(
+            az=[10 << u.deg, 350 << u.deg], el=[15 << u.deg, 80 << u.deg]
+        ),
+        "antenna_pointing_accuracy": 10 << u.arcsec,
+        "ros_service_timeout_sec": 10,
     }
     expected_custom_config = {
         "observatory": "NANTEN2",
@@ -45,13 +56,27 @@ class TestConfigure:
             lon="-67.70308139deg", lat="-22.96995611deg", height="4863.85m"
         ),
         "not_supported_entry": 1,
+        "antenna_pid_param": SimpleNamespace(az=[2.2, 0.0, 0.0], el=[2.2, 0, 0.0]),
+        "antenna_drive_range": SimpleNamespace(
+            az=[-270, 270] << u.deg, el=[0, 90] << u.deg
+        ),
+        "antenna_drive_softlimit": SimpleNamespace(
+            az=[-240, 240] << u.deg, el=[20, 80] << u.deg
+        ),
+        "antenna_pointing_accuracy": 3 << u.arcsec,
+        "ros_service_timeout_sec": 10,
     }
 
     def test_configure_no_config(self, dot_necst_dir: Path):
         assert not (dot_necst_dir / "config.toml").exists()
 
         config = configure()
-        assert config.__dict__ == self.expected_default_config
+        for actual, expected in zip(config.__dict__, self.expected_default_config):
+            try:
+                equality = actual == expected
+                assert equality if isinstance(equality, bool) else all(equality)
+            except ValueError:
+                print("Couldn't determine equality of encapsulated sequence")
         assert (dot_necst_dir / "config.toml").exists()
 
     def test_configure_no_env(self, data_dir: Path, dot_necst_dir: Path):
@@ -61,7 +86,12 @@ class TestConfigure:
         assert (dot_necst_dir / "config.toml").exists()
 
         config = configure()
-        assert config.__dict__ == self.expected_custom_config
+        for actual, expected in zip(config.__dict__, self.expected_custom_config):
+            try:
+                equality = actual == expected
+                assert equality if isinstance(equality, bool) else all(equality)
+            except ValueError:
+                print("Couldn't determine equality of encapsulated sequence")
 
     def test_configure_with_env_but_no_config(
         self, data_dir: Path, dot_necst_dir: Path, custom_necst_root_dir: Path
@@ -74,7 +104,12 @@ class TestConfigure:
         assert not (custom_necst_root_dir / "config.toml").exists()
 
         config = configure()
-        assert config.__dict__ == self.expected_custom_config
+        for actual, expected in zip(config.__dict__, self.expected_custom_config):
+            try:
+                equality = actual == expected
+                assert equality if isinstance(equality, bool) else all(equality)
+            except ValueError:
+                print("Couldn't determine equality of encapsulated sequence")
 
     def test_configure_with_env(
         self, data_dir: Path, dot_necst_dir: Path, custom_necst_root_dir: Path
@@ -89,4 +124,9 @@ class TestConfigure:
         assert (dot_necst_dir / "config.toml").exists()
 
         config = configure()
-        assert config.__dict__ == self.expected_custom_config
+        for actual, expected in zip(config.__dict__, self.expected_custom_config):
+            try:
+                equality = actual == expected
+                assert equality if isinstance(equality, bool) else all(equality)
+            except ValueError:
+                print("Couldn't determine equality of encapsulated sequence")
