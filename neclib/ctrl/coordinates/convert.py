5a313e992561d89b35ca5e057e39145ee7f14467
Kirby1717 <64296621+Kirby1717@users.noreply.github.com>
2022-09-02 00:08:34 +0900

    #112 Update functions

--- neclib/ctrl/coordinates/convert.py
+++ neclib/ctrl/coordinates/convert.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 from typing import TypeVar, Union
+import logging
 
 import astropy.units as u
 import numpy as np
-from astropy.coordinates import BaseCoordinateFrame, EarthLocation
+from astropy.coordinates import BaseCoordinateFrame, EarthLocation, SkyCoord, ICRS, Galactic, FK4, FK5, AltAz
 from astropy.time import Time
 
 
 T = TypeVar("T", float, np.ndarray, u.Quantity)
 # lon と lat の型が揃っていない場合には対応しなくていい
 
-Number = Union[int, float, np.ndarray]
+Number = Union[int, float, np.ndarray] # int または float または np.ndarray
 PathLike = Union[str, bytes, Path]
 
 
@@ -19,14 +20,28 @@ class CoordCalculator:
         self,
         location: EarthLocation,
         pointing_param_path: PathLike,
-        *,
+        *, # 以降の引数はキーワード引数（引数名=値）として受け取ることを強制する
         pressure: u.Quantity = None,
         temperature: u.Quantity = None,
         relative_humidity: Union[Number, u.Quantity] = None,
         obswl: u.Quantity = None,
     ) -> None:
+        self.location = location
+        self.pointing_param_path = pointing_param_path
+        self.pressure = pressure
+        self.temperature = temperature
+        self.relative_humidity = relative_humidity
+        self.obswl = obswl
         # pressure, temperature, relative_humidity, obswl のいずれかが指定されていなかったら logging で warning
-        ...
+        logger = logging.getLogger(__name__)
+        if pressure is None:
+            logger.warning("pressure が未指定です。")
+        if temperature is None:
+            logger.warning("temperature が未指定です。")
+        if relative_humidity is None:
+            logger.warning("relative_humidity が未指定です。")
+        if obswl is None:
+            logger.warning("obswl が未指定です。")
 
     def update_weather(
         self, 
@@ -34,7 +49,10 @@ class CoordCalculator:
         temperature: u.Quantity = None,
         relative_humidity: Union[Number, u.Quantity] = None,
     ) -> None:
-        ...
+        """気象情報を更新する"""
+        self.pressure = pressure
+        self.temperature = temperature
+        self.relative_humidity = relative_humidity
 
     def get_horizontal_by_name(
         self,
@@ -42,7 +60,17 @@ class CoordCalculator:
         *,
         obstime: Union[Number, Time],
     ) -> u.Quantity:
-        ...
+        """天体名から地平座標 az, el(alt) を取得する"""
+        radec = SkyCoord.from_name(name)
+        altaz = radec.transform_to(AltAz(
+            obstime = obstime,
+            location = self.location,
+            pressure = self.pressure,
+            temperature = self.temperature,
+            relative_humidity = self.relative_humidity,
+            obswl = self.obswl,
+        ))
+        return altaz.az, altaz.alt
 
     def get_horizontal(
         self,
