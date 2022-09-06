9a80f8dd43c97edfa03d1195b2a8a86634f2d1e6
Kirby1717 <64296621+Kirby1717@users.noreply.github.com>
2022-09-06 11:26:48 +0900

    #112 Add comments

--- neclib/ctrl/coordinates/convert.py
+++ neclib/ctrl/coordinates/convert.py
@@ -74,13 +74,13 @@ class CoordCalculator:
 
     def get_horizontal(
         self,
-        lon: T,
-        lat: T,
-        frame: Union[str, BaseCoordinateFrame],
+        lon: T, # 変換前の経度
+        lat: T, # 変換前の緯度
+        frame: Union[str, BaseCoordinateFrame], # 変換前の座標系
         *,
         unit: Union[str, u.Unit] = None,
         obstime: Union[Number, Time],
-    ) -> u.Quantity:
+    ) -> u.Quantity: # 物理量（数値 * 単位）
         # T が u.Quantity でなければ unit 指定必須
         # T が u.Quantity ならば unit に変換して返す
         ...
