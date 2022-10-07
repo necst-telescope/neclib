bea7c5b173c61cd39647536b041d187220be3e69
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-10-07 17:07:05 +0900

    #174 Control both Az. and El. axes

--- neclib/devices/encoder/nd287.py
+++ neclib/devices/encoder/nd287.py
@@ -1,30 +1,29 @@
 __all__ = ["ND287"]
 
+from typing import Literal
+
 import astropy.units as u
 import ogameasure
 
+from ... import config
 from .encoder_base import Encoder
 
 
 class ND287(Encoder):
-    """Encoder readout.
-
-    Parameters
-    ----------
-    port
-        Device path in UNIX file system, like ``"/dev/ttyUSB1"``.
-
-    """
+    """Encoder readout."""
 
     Manufacturer = "HEIDENHAIN"
     Model = "ND287"
 
-    def __init__(self, port: str) -> None:
-        self.driver = ogameasure.HEIDENHAIN.ND287(port)
+    def __init__(self) -> None:
+        self.driver = {
+            "az": ogameasure.HEIDENHAIN.ND287(config.antenna_nd287_az),
+            "el": ogameasure.HEIDENHAIN.ND287(config.antenna_nd287_el),
+        }
 
-    def get_reading(self) -> u.Quantity:
-        raw = self.driver.output_position_display_value()
-        return float(raw.strip(b"\x02\x00\r\n").decode())
+    def get_reading(self, axis: Literal["az", "el"]) -> u.Quantity:
+        raw = self.driver[axis.lower()].output_position_display_value()
+        return float(raw.strip(b"\x02\x00\r\n").decode()) << u.deg
 
     def finalize(self) -> None:
         pass
