930e5da5b72cafcb83615e390656ca79682215ee
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-10-09 18:45:27 +0900

    #181 Implement recorder

--- neclib/recorders/__init__.py
+++ neclib/recorders/__init__.py
@@ -1,1 +1,5 @@
-from .db_writer import DBWriter  # noqa: F401, F403
+# Core recorder
+from .recorder import Recorder  # noqa: F401
+
+# Writers
+from .db_writer import DBWriter  # noqa: F401
---
+++ neclib/recorders/recorder.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from pathlib import Path
+from typing import List, Union
+
+from .writer_base import Writer
+from ..typing import PathLike
+from .. import get_logger
+
+
+class Recorder:
+    """Data recorder, to be used with arbitrary writers.
+
+    Parameters
+    ----------
+    record_root
+        Root directory of data storage. All the data will be stored inside this
+        directory. Subdirectories will be structured by attached writers.
+
+    Examples
+    --------
+    >>> recorder = neclib.recorders.Recorder("/home/user/data")
+    >>> recorder.add_writer(neclib.recorders.DBWriter(), neclib.recorders.FileWriter())
+    >>> recorder.start_recording()
+    >>> recorder.append("test", 1)  # Arbitrary number and type of data can be passed
+    >>> recorder.stop_recording()
+
+    """
+
+    def __init__(self, record_root: Path) -> None:
+        self.__writers: List[Writer] = []
+        self.record_root = record_root
+        self.recording_path = None
+
+        self.logger = get_logger(self.__class__.__name__)
+
+    def add_writer(self, *writers: Writer) -> None:
+        """Attach writer(s) to this recorder."""
+        if any(type(w) is type for w in writers):
+            raise TypeError("Writer should be instantiated.")
+        self.__writers.extend(writers)
+
+    @property
+    def writers(self):
+        """List of attached writers."""
+        return self.__writers
+
+    def drop_writer(self, *writers: Union[int, Writer]) -> None:
+        """Drop writer(s) from this recorder."""
+        for writer in writers:
+            if isinstance(writer, int):
+                to_remove = self.__writers.pop(writer)
+                to_remove.stop_recording()
+            else:
+                writer.stop_recording()
+                self.__writers.remove(writer)
+
+    def start_recording(self, record_dir: PathLike) -> None:
+        """Activate all attached writers."""
+        if record_dir is not None:
+            self.recording_path = Path(record_dir)
+        else:
+            self.recording_path = self._auto_generate_record_dir()
+
+        for writer in self.__writers:
+            writer.start_recording(self.recording_path)
+
+    def append(self, *args, **kwargs) -> None:
+        """Pass data to all attached writers."""
+        if self.recording_path is None:
+            raise RuntimeError("Recorder not started. Incoming data won't be kept.")
+        handled = [writer.append(*args, **kwargs) for writer in self.__writers]
+        if not any(handled):
+            self.logger.warning(
+                f"No writer handled the data ({args, kwargs}). Data will be lost."
+            )
+
+    def stop_recording(self) -> None:
+        """Deactivate all attached writers."""
+        for writer in self.__writers:
+            writer.stop_recording()
+        self.recording_path = None
+
+    def _auto_generate_record_dir(self) -> Path:
+        now = datetime.utcnow()
+        record_dir = self.record_root / now.strftime("%Y%m") / now.strftime("%Y%m%d")
+        record_dir.mkdir(parents=True, exist_ok=True)
+        return record_dir
