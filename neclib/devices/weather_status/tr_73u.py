9f10e26cc7a901be5058c5b39d9ef8af6e8e231e
Fuuna Takayama <kaede.awayuki@gmail.com>
2022-10-17 07:25:11 +0000

    add comment

--- neclib/devices/weather_status/tr_73u.py
+++ neclib/devices/weather_status/tr_73u.py
@@ -11,12 +11,12 @@ class Thermometer(Weather):
     Manufacturer = ""
     Model = "TR_73U"
 
-    def __init__(self) -> None:
+    def __init__(self) -> None: #initを作っただけ。中に何を書くべきか考え中
         ...
     
     def read(self) -> Float64:
-        while not rospy.is_shutdown():
-            data = self.ondotori.output_current_data()
+        data = self.ondotori.output_current_data() #元のプログラムから移しただけ。まだ中身が理解できていない
+        while not rclpy.is_shutdown():
             try:
                 d = struct.unpack('26B', data)
                 temp = (d[6]*16**2+d[5]-1000)/10
