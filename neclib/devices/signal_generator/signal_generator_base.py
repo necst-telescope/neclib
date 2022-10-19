62f257231d983635ea1ef89525751d52f8c49f61
Yamato Matsuzuki <y.matsuzuki@a.phys.nagoya-u.ac.jp>
2022-10-19 22:28:22 +0900

    #187 add signal_generator_base.py

---
+++ neclib/devices/signal_generator/signal_generator_base.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Literal
+
+
+class SignalGenerator(ABC):
+
+    Manufacturer: str = ""
+    Model: str
+
+    @abstractmethod
+    def set_freq(self, freq_GHz) -> None:
+        ...
+
+    @abstractmethod
+    def set_power(self, power_dBm) -> None:
+        ...
+
+    @abstractmethod
+    def get_freq(self) -> float:
+        ...
+
+    @abstractmethod
+    def get_power(self) -> float:
+        ...
+
+    @abstractmethod
+    def set_onoff(self, q) -> None:
+        ...
