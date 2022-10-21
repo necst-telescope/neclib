aa5dc47bf711e41bdac6ddd0904c8dced2be0705
Yamato Matsuzuki <y.matsuzuki@a.phys.nagoya-u.ac.jp>
2022-10-21 22:27:36 +0900

    #187 add fsw0010.py, fsw0020.py

---
+++ neclib/devices/signal_generator/fsw0010.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python3
+
+name = "fsw0010"
+
+import time
+import sys
+import ogameasure
+import threading
+from ... import config
+
+from .signal_generator_base import SignalGenerator
+
+class fsw0010(SignalGenerator):
+
+    Manufacturer: str = "Phasematrix"
+    Model = "fsw0010"
+
+    def __init__(self):
+        self.communicating = True
+        com = ogameasure.ethernet(config.rx_fsw0010_host, config.rx_fsw0010_port)
+        self.sg = ogameasure.Phasematrix.FSW0010(com)
+        self.sg.use_external_reference_source()
+
+    def set_freq(self,freq_GHz):
+        if self.communicating == False:
+            self.sg.freq_set(freq_GHz)
+            time.sleep(1)
+            self.communicating = True
+            return
+        elif self.communicating == True:
+            self.communicating = False
+            self.sg.freq_set(freq_GHz)
+            time.sleep(1)
+            self.communicating = True
+            return
+
+    def set_power(self, power_dBm):
+        if self.communicating == False:
+            self.sg.power_set(power_dBm)
+            time.sleep(1)
+            self.communicating = True
+            return
+        elif self.communicating == True:
+            self.communicating = False
+            self.sg.power_set(power_dBm)
+            time.sleep(1)
+            self.communicating = True
+            return
+
+    def set_onoff(self, onoff):
+        if self.communicating == False:
+            self.sg.output_set(onoff)
+            time.sleep(1)
+            self.communicating = True
+            return
+        elif self.communicating == True:
+            self.communicating = False
+            self.sg.output_set(onoff)
+            time.sleep(1)
+            self.communicating = True
+            return
+
+    def get_freq(self):
+        if self.communicating == False:
+            self.sg.freq_query()
+            time.sleep(1)
+            self.communicating = True
+            return
+        elif self.communicating == True:
+            self.communicating = False
+            self.sg.freq_query()
+            time.sleep(1)
+            self.communicating = True
+            return
+    
+    def get_power(self):
+        if self.communicating == False:
+            self.sg.power_query()
+            time.sleep(1)
+            self.communicating = True
+            return
+        elif self.communicating == True:
+            self.communicating = False
+            self.sg.power_query()
+            time.sleep(1)
+            self.communicating = True
+            return
+
+    
