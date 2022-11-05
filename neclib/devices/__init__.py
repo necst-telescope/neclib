624944d8a4d3ffa255213238729e0f02fb4a01ac
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-11-05 12:33:35 +0900

    #234 Change variable to use for refer to current package

--- neclib/devices/__init__.py
+++ neclib/devices/__init__.py
@@ -6,7 +6,9 @@ from . import selector
 
 paths = Path(__file__).parent.iterdir()
 module_paths = filter(lambda p: p.is_dir() and p.name[0] not in "._", paths)
-impl_modules = [importlib.import_module(f".{m.name}", __name__) for m in module_paths]
+impl_modules = [
+    importlib.import_module(f".{m.name}", __package__) for m in module_paths
+]
 
 implementations = selector.list_implementations(impl_modules)
 """List of all available implementations."""
