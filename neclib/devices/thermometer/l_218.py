9ba84c9686a42f3a316b520631dbaa2a9bef0f34
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2022-11-11 17:38:08 +0900

    #247 delete Japanese notation

--- neclib/devices/thermometer/l_218.py
+++ neclib/devices/thermometer/l_218.py
@@ -13,14 +13,13 @@ class L218(Thermometer):
 
     def __init__(self) -> None:
         com = ogameasure.gpib_prologix(
-            config.rx_l218_host, gpibport=config.rx_l218_gpibadrr
+            host=config.rx_l218_host, gpibport=config.rx_l218_gpibadrr
         )
         self.thermometer = ogameasure.Lakeshore.model218(com)
-        # GPIBで接続できるようにすること。
 
     def get_temp(self) -> u.Quantity:
         data = self.thermometer.kelvin_reading_query()
         return data * u.K
 
     def finalize(self) -> None:
-        self.thermometer.close()
+        self.thermometer.com.close()
