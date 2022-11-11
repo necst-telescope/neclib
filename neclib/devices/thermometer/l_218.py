f52dbb09f3c4adc3288db766cf0b99cb86519fd9
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2022-11-11 17:29:47 +0900

    #247 1st write class code

--- neclib/devices/thermometer/l_218.py
+++ neclib/devices/thermometer/l_218.py
@@ -1,2 +1,26 @@
-class L218:
-    ...
+import astropy.units as u
+
+import ogameasure
+
+from ... import config
+from .thermometer_base import Thermometer
+
+
+class L218(Thermometer):
+
+    Manufacturer = "Lakeshore"
+    Model = "Model218"
+
+    def __init__(self) -> None:
+        com = ogameasure.gpib_prologix(
+            config.rx_l218_host, gpibport=config.rx_l218_gpibadrr
+        )
+        self.thermometer = ogameasure.Lakeshore.model218(com)
+        # GPIBで接続できるようにすること。
+
+    def get_temp(self) -> u.Quantity:
+        data = self.thermometer.kelvin_reading_query()
+        return data * u.K
+
+    def finalize(self) -> None:
+        self.thermometer.close()
