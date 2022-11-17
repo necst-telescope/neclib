eef3e4eabe77f12048ec6fe0bf4f9a145fb161f8
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-11-17 15:45:37 +0900

    #209 Fix documentation style

--- neclib/simulators/__init__.py
+++ neclib/simulators/__init__.py
@@ -5,11 +5,11 @@ Notes
 NECST modules should support simulator mode. Here is a collection of modules necessary
 to implement the mode.
 
-.. note::
-
-    If there's a corresponding non-simulator script, functionalities in the original one
-    should be used as much as possible, through inheriting and overriding, to ensure the
-    behavior is the same as real one.
+Important
+---------
+If there's a corresponding non-simulator script, functionalities in the original one
+should be used as much as possible, through inheriting and overriding, to ensure the
+behavior is the same as real one.
 
 """
 
