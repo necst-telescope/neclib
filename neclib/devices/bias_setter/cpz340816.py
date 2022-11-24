e5ce0eeeff8e798dce8fb5ef61eb8b0c1e2362a2
Yamato Matsuzuki <y.matsuzuki@a.phys.nagoya-u.ac.jp>
2022-11-24 23:47:15 +0900

    #262 Add error message and change function name

--- neclib/devices/bias_setter/cpz340816.py
+++ neclib/devices/bias_setter/cpz340816.py
@@ -1,4 +1,5 @@
 import pyinterface
+
 from ...utils import busy
 from .bias_setter_base import BiasSetter
 
@@ -32,35 +33,17 @@ class CPZ340816(BiasSetter):
         }
         self.da = pyinterface.open(3408, self.rsw_id)
 
-    def set_param(self, voltage_V: float, ch: int) -> None:
-        with busy(self, "busy"):
+    def set_voltage(self, voltage_V: float, ch: int) -> None:
+        if ch not in self.param_buff.keys():
+            raise ValueError("Invaild channel {ch}")
+        else:
             self.param_buff[ch] = voltage_V
 
     def output_voltage(self) -> None:
         with busy(self, "busy"):
-            param_buff = self.param_buff.copy()
-            self.param_buff = {
-                1: 0.0,
-                2: 0.0,
-                3: 0.0,
-                4: 0.0,
-                5: 0.0,
-                6: 0.0,
-                7: 0.0,
-                8: 0.0,
-                9: 0.0,
-                10: 0.0,
-                11: 0.0,
-                12: 0.0,
-                13: 0.0,
-                14: 0.0,
-                15: 0.0,
-                16: 0.0,
-            }
-
             for i in range(0, 16):
-                ch = int(list(param_buff.keys())[i])
-                voltage = list(param_buff.values())[i]
+                ch = int(list(self.param_buff.keys())[i])
+                voltage = list(self.param_buff.values())[i]
 
                 self.da.output_da(ch, voltage)
 
