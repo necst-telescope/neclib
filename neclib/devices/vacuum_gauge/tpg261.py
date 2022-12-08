154e96e73d6f6cadf67546a6621cbcde19a1f92f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-09 04:45:16 +0900

    #281 Add vacuum gauge controller

---
+++ neclib/devices/vacuum_gauge/tpg261.py
@@ -0,0 +1,22 @@
+import astropy.units as u
+import ogameasure
+
+from .vacuum_gauge_base import VacuumGauge
+
+
+class TPG261(VacuumGauge):
+
+    Model = "TPG261"
+    Manufacturer = "Pfeiffer"
+
+    Identifier = "host"
+
+    def __init__(self):
+        com = ogameasure.ethernet(self.Config.host, self.Config.port)
+        self.io = ogameasure.Pfeiffer.tpg261_lan(com)
+
+    def get_pressure(self) -> u.Quantity:
+        return self.io.pressure() * u.hPa
+
+    def finalize(self):
+        self.io.com.close()
