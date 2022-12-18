6812de5e5edbc534e70b9491ae5bcbbfcfae4b13
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-18 18:00:05 +0900

    #329 Add test suite ofor file save functionality

--- tests/utils/test_file_utils.py
+++ tests/utils/test_file_utils.py
@@ -1,4 +1,14 @@
+from pathlib import Path
+
+import pytest
+
 from neclib.utils import read_file
+
+
+@pytest.fixture
+def home_dir(tmp_path_factory) -> Path:
+    home = tmp_path_factory.mktemp("username")
+    return home
 
 
 class TestReadFile:
@@ -6,3 +16,9 @@ class TestReadFile:
         assert read_file(data_dir / "sample_config_customized.toml").startswith(
             'observatory = "NANTEN2"'
         )
+
+    def test_read_text_then_write(self, home_dir, data_dir):
+        read_file(
+            data_dir / "sample_config_customized.toml", saveto=home_dir / "config.toml"
+        )
+        assert read_file(home_dir / "config.toml").startswith('observatory = "NANTEN2"')
