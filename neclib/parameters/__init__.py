896667eab9fdc2ce241484a7b3bf44bf097ffe26
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-18 17:52:07 +0900

    #329 Reorganize import order

--- neclib/parameters/__init__.py
+++ neclib/parameters/__init__.py
@@ -1,8 +1,5 @@
 """Define and handle parameters."""
 
-# Alias
 from .obsparam import *  # noqa: F401, F403
-from .pointing_error import *  # noqa: F401, F403
-
-# Subpackage
 from .parser import *  # noqa: F401, F403
+from .pointing_error import *  # noqa: F401, F403
