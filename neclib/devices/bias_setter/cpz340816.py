38ee16ba4ea86c006f4da2797390ff71604e0302
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-22 22:06:53 +0900

    #348 Add safety measures

--- neclib/devices/bias_setter/cpz340816.py
+++ neclib/devices/bias_setter/cpz340816.py
@@ -13,31 +13,17 @@ class CPZ340816(BiasSetter):
         import pyinterface
 
         self.rsw_id = self.Config.rsw_id
-        self.param_buff = {
-            1: 0.0,
-            2: 0.0,
-            3: 0.0,
-            4: 0.0,
-            5: 0.0,
-            6: 0.0,
-            7: 0.0,
-            8: 0.0,
-            9: 0.0,
-            10: 0.0,
-            11: 0.0,
-            12: 0.0,
-            13: 0.0,
-            14: 0.0,
-            15: 0.0,
-            16: 0.0,
-        }  # All in [V]
+        self.param_buff = {i: 0.0 for i in range(1, 17)}  # All in [V]
         self.da = pyinterface.open(3408, self.rsw_id)
 
-    def set_voltage(self, voltage_mV: float, ch: int) -> None:
+    def set_voltage(self, mV: float, id: str) -> None:
+        ch = self.Config.device_id[id]
         if ch not in self.param_buff.keys():
-            raise ValueError("Invaild channel {ch}")
+            raise ValueError(f"Invaild channel {ch}")
+        if self.Config.max_mv[0] < mV < self.Config.max_mv[1]:
+            raise ValueError(f"Unsafe voltage {mV} mV")
         else:
-            self.param_buff[ch] = voltage_mV / 3
+            self.param_buff[ch] = mV / 3
 
     def output_voltage(self) -> None:
         with busy(self, "busy"):
@@ -47,25 +33,6 @@ class CPZ340816(BiasSetter):
 
                 self.da.output_da(ch, voltage)
 
-                continue
-
     def finalize(self) -> None:
         self.da.finalize()
-        self.param_buff = {
-            1: 0.0,
-            2: 0.0,
-            3: 0.0,
-            4: 0.0,
-            5: 0.0,
-            6: 0.0,
-            7: 0.0,
-            8: 0.0,
-            9: 0.0,
-            10: 0.0,
-            11: 0.0,
-            12: 0.0,
-            13: 0.0,
-            14: 0.0,
-            15: 0.0,
-            16: 0.0,
-        }
+        self.param_buff = {i: 0.0 for i in range(1, 17)}
