182a73c30b6cf066268a1ab09c5a266debb84082
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-25 17:44:02 +0900

    #353 Fix term for attenuator control

--- neclib/devices/attenuator/rhio10.py
+++ neclib/devices/attenuator/rhio10.py
@@ -17,15 +17,17 @@ class RHIO10(Attenuator):
         com = ogameasure.ethernet(self.Config.host, self.Config.port)
         self.io = ogameasure.SENA.adios(com)
 
-    def get_level(self, ch: Literal[1, 2]) -> u.Quantity:
+    def get_loss(self, id: Literal[1, 2]) -> u.Quantity:
+        ch = self.Config.device_id[id]
         if ch == 1:
             return self.io.get_att1() << u.dB
         elif ch == 2:
             return self.io.get_att2() << u.dB
         raise ValueError(f"Invalid channel number: {ch}")
 
-    def set_level(self, level_dB: int, ch: Literal[1, 2]) -> None:
-        self.io._set_att(ch, int(level_dB))
+    def set_loss(self, dB: int, id: Literal[1, 2]) -> None:
+        ch = self.Config.device_id[id]
+        self.io._set_att(ch, int(dB))
 
     def finalize(self) -> None:
         self.io.com.close()
