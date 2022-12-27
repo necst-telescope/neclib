a02355980ad4d09aeaed863acbbf43407da72a6f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-27 17:07:26 +0900

    #353 Change test targets

--- tests/devices/test_selector.py
+++ tests/devices/test_selector.py
@@ -4,10 +4,16 @@ from neclib.utils import toCamelCase
 
 class TestSelector:
     def test_camelcase_import(self):
-        for k, v in config.dev.items():
+        configured_devices = {
+            k.split("::")[0]: v for k, v in config.items() if k.endswith("::_")
+        }
+        for k, v in configured_devices.items():
             name = toCamelCase(k)
-            assert getattr(devices, name).Model == v
+            assert v in repr(getattr(devices, name))
 
     def test_snakecase_import(self):
-        for name, v in config.dev.items():
-            assert getattr(devices, name).Model == v
+        configured_devices = {
+            k.split("::")[0]: v for k, v in config.items() if k.endswith("::_")
+        }
+        for name, v in configured_devices.items():
+            assert v in repr(getattr(devices, name))
