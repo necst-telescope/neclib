4fdfdea74946027c9a45e5e81cf1abae41b6a5b5
KaoruNishikawa <linersky246@gmail.com>
2023-02-01 03:14:23 +0900

    #409 Add type aliases

--- neclib/core/type_aliases.py
+++ neclib/core/type_aliases.py
@@ -1,10 +1,19 @@
-from typing import Tuple, Union
+from typing import Callable, Tuple, Union
 
 import numpy as np
-from astropy.units import Unit
+from astropy.coordinates import BaseCoordinateFrame
+from astropy.units import UnitBase
 
 DimensionLess = Union[int, float, np.ndarray[Tuple[int, ...], np.dtype[np.number]]]
 """Type alias for values with no physical units."""
 
-UnitType = Union[Unit, str]
+UnitType = Union[UnitBase, str]
 """Type alias for objects that represents physical unit."""
+
+CoordFrameType = Union[str, BaseCoordinateFrame]
+"""Type alias for objects that represents coordinate frame."""
+
+EquivalencyType = Tuple[
+    UnitBase, UnitBase, Callable[[float], float], Callable[[float], float]
+]
+"""Type alias for unit equivalency in ``astropy.units``."""
