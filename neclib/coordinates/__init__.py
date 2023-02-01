ba4e7dfe3b4e7ee2f7bc6894a821e7f74e53bbae
KaoruNishikawa <linersky246@gmail.com>
2023-02-02 05:12:46 +0900

    #409 Reorganize pointing error modules

--- neclib/coordinates/__init__.py
+++ neclib/coordinates/__init__.py
@@ -3,3 +3,4 @@ from .frame import *  # noqa: F401, F403
 from .observer import *  # noqa: F401, F403
 from .optimize import *  # noqa: F401, F403
 from .path_finder import *  # noqa: F401, F403
+from .pointing_error import *  # noqa: F401, F403
