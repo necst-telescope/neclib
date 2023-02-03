597feba5b0087c09a56d26b1cc92b209624a4ea4
KaoruNishikawa <linersky246@gmail.com>
2023-02-04 02:49:41 +0900

    #409 Rename test suite

--- tests/core/formatting/test_html_repr_of_dict.py
+++ tests/core/formatting/test_html_repr_of_dict.py
@@ -22,13 +22,13 @@ class TestHTMLReprOfDict:
         assert html_repr_of_dict({}, float) == expected
 
         expected = """\
-<span>Ellipsis</span><hr><details><summary>0 elements</summary>\
+<span>builtins.NoneType</span><hr><details><summary>0 elements</summary>\
 <table><thead><tr><th>Key</th><th>Value</th><th>Type</th></tr></thead><tbody></tbody>\
 </table></details><details><summary>0 elements have alias key(s)</summary><table>\
 <thead><tr><th>Key</th><th>Aliases</th></tr></thead><tbody></tbody></table></details>\
 <details><summary>0 metadata</summary><table><thead><tr><th>Key</th><th>Value</th>\
 <th>Type</th></tr></thead><tbody></tbody></table></details>"""
-        assert html_repr_of_dict({}, ...) == expected
+        assert html_repr_of_dict({}, type(None)) == expected
 
     def test_element_repr(self) -> None:
         expected = """\
