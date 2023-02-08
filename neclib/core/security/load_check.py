bc7a67943bcf96903c5842902df338a1a8f61512
KaoruNishikawa <linersky246@gmail.com>
2023-02-08 18:07:58 +0900

    #404 Add computer load checker

---
+++ neclib/core/security/load_check.py
@@ -0,0 +1,51 @@
+import time
+from typing import Dict, Optional
+
+import astropy.units as u
+import psutil
+from astropy.units import Quantity
+
+
+class LoadChecker:
+    cpu_count = psutil.cpu_count()
+
+    def __init__(self):
+        self._last_netcount: Optional[Dict[str, float]] = None
+
+    def cpu_usage(self) -> Quantity:
+        """Mean CPU usage since last call.
+
+        Since this method averages the usage from last call, the initial call returns
+        meaningless 0.
+
+        """
+        usage = psutil.cpu_percent(interval=None, percpu=True)
+        if usage is None:
+            return [0] * self.cpu_count * u.percent
+        return usage * u.percent
+
+    def memory_available(self) -> Quantity:
+        """Available memory."""
+        return psutil.virtual_memory().available * u.byte
+
+    def disk_usage(self) -> Quantity:
+        """Disk usage."""
+        return psutil.disk_usage("/").percent * u.percent
+
+    def network_amount(self) -> Dict[str, Quantity]:
+        """Network communication amount."""
+        netcount = psutil.net_io_counters()
+        current_netcount: Dict[str, float] = {
+            "sent": netcount.bytes_sent,
+            "recv": netcount.bytes_recv,
+            "time": time.monotonic(),
+        }
+        if self._last_netcount is None:
+            self._last_netcount = current_netcount
+            return {"sent": 0 * u.byte / u.s, "recv": 0 * u.byte / u.s}
+
+        dt = current_netcount["time"] - self._last_netcount["time"]
+        dsent = netcount.bytes_sent - self._last_netcount["sent"]
+        drecv = netcount.bytes_recv - self._last_netcount["recv"]
+        self._last_netcount = current_netcount
+        return {"sent": dsent / dt * u.byte / u.s, "recv": drecv / dt * u.byte / u.s}
