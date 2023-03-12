b886a0fa86cc657406ba70c42cbaeaed6393f3fb
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2023-03-12 19:53:15 +0900

    #435 Remove subpackage-level __all__
    To avoid docs cross reference ambiguity

--- neclib/core/data_type/__init__.py
+++ neclib/core/data_type/__init__.py
@@ -1,9 +1,7 @@
 """Collection of data type definitions this package interprets."""
 
-from .ordinal import Ordinal
-from .parameters import Parameters
-from .rich_parameters import RichParameters
-from .status_manager import StatusManager
-from .value_range import ValueRange
-
-__all__ = ["Ordinal", "Parameters", "RichParameters", "ValueRange", "StatusManager"]
+from .ordinal import Ordinal  # noqa: F401
+from .parameters import Parameters  # noqa: F401
+from .rich_parameters import RichParameters  # noqa: F401
+from .status_manager import StatusManager  # noqa: F401
+from .value_range import ValueRange  # noqa: F401
