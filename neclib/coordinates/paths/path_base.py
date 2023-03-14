b5c148cd4b77c16ae53c1f9c580805975efaf853
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2023-03-14 16:30:08 +0900

    #435 Add common method to calculate drive path

--- neclib/coordinates/paths/path_base.py
+++ neclib/coordinates/paths/path_base.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
+import time
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Dict, Generator, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Dict, Generator, Optional, Tuple, Union, overload
 
 import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
 
 from ...core.normalization import NPArrayValidator
-from ...core.types import DimensionLess
+from ...core.types import CoordFrameType, DimensionLess, UnitType
 
 if TYPE_CHECKING:
     from ..convert import CoordCalculator
@@ -22,13 +25,131 @@ class Path(ABC):
     infinite: bool
     waypoint: bool
 
-    def __init__(self, calc: CoordCalculator, *args, **kwargs) -> None:
+    def __init__(
+        self, calc: CoordCalculator, *target, unit: Optional[UnitType] = None, **kwargs
+    ) -> None:
         self._calc = calc
+
+        if len(target) == 0:
+            _target = None
+        elif (len(target) == 1) and isinstance(target[0], str):
+            _target = target[0]
+        elif len(target) == 3:
+            lon, lat, frame = target
+            _target = self.get_skycoord(lon, lat, frame, unit=unit)  # type: ignore
+        else:
+            raise TypeError(
+                "Invalid number of positional arguments: expected 0 (none, other "
+                "arguments specify the absolute coordinate), 1 (target_name) or "
+                f"3 (lon, lat, coordinate_frame), but got {len(target)}"
+            )
+        self._target = _target
 
     @property
     @abstractmethod
     def arguments(self) -> Tuple[Tuple[Any, ...], Dict[str, Any]]:
         ...
+
+    @property
+    def target_frame(self) -> Optional[CoordFrameType]:
+        if self._target is None:
+            return None
+        elif isinstance(self._target, SkyCoord):
+            frame = self._target.frame
+        else:
+            frame = self._calc.get_body(self._target, time.time()).frame
+
+        if "obstime" in frame.frame_attributes:
+            frame = frame.replicate_without_data(obstime=None)
+
+        return frame
+
+    @overload
+    def get_skycoord(self, /, *, obstime: Any = None, unit: Any = None) -> None:
+        ...
+
+    @overload
+    def get_skycoord(
+        self, coord: SkyCoord, /, *, obstime: Any = None, unit: Any = None
+    ) -> SkyCoord:
+        ...
+
+    @overload
+    def get_skycoord(
+        self, coord: str, /, *, obstime: Union[Time, DimensionLess], unit: Any = None
+    ) -> SkyCoord:
+        ...
+
+    @overload
+    def get_skycoord(
+        self,
+        lon: T,
+        lat: T,
+        frame: CoordFrameType,
+        /,
+        *,
+        obstime: Optional[Union[Time, DimensionLess]] = None,
+        unit: Optional[UnitType] = None,
+    ) -> SkyCoord:
+        ...
+
+    def get_skycoord(
+        self,
+        *coord: Union[str, T, CoordFrameType, SkyCoord],
+        obstime: Optional[Union[Time, DimensionLess]] = None,
+        unit: Optional[UnitType] = None,
+    ) -> Optional[SkyCoord]:
+        if len(coord) == 0:
+            return
+        elif (len(coord) == 1) and isinstance(coord[0], SkyCoord):
+            return coord[0]
+        elif (len(coord) == 1) and isinstance(coord[0], str) and (obstime is not None):
+            return self._calc.get_body(coord[0], obstime)
+        elif len(coord) == 3:
+            lon, lat, frame = coord
+            return self._calc.create_skycoord(
+                lon, lat, frame=frame, obstime=obstime, unit=unit
+            )
+        raise TypeError(f"Unexpected argument types: {type(coord)=}, {type(obstime)=}")
+
+    @overload
+    def apply_offset(
+        self,
+        coord: SkyCoord,
+        /,
+        *,
+        offset: Optional[Tuple[T, T, CoordFrameType]] = None,
+    ) -> SkyCoord:
+        ...
+
+    @overload
+    def apply_offset(
+        self,
+        *coords: SkyCoord,
+        offset: Optional[Tuple[T, T, CoordFrameType]] = None,
+        obstime: Optional[Union[Time, DimensionLess]] = None,
+        unit: Optional[UnitType] = None,
+    ) -> Tuple[SkyCoord, ...]:
+        ...
+
+    def apply_offset(
+        self,
+        *coords: SkyCoord,
+        offset: Optional[Tuple[T, T, CoordFrameType]] = None,
+        obstime: Optional[Union[Time, DimensionLess]] = None,
+        unit: Optional[UnitType] = None,
+    ) -> Union[Tuple[SkyCoord, ...], SkyCoord]:
+        if offset is None:
+            return coords if len(coords) > 1 else coords[0]
+
+        offset_applied = []
+        for coord in coords:
+            applied = self._calc.cartesian_offset_by(
+                coord, *offset, obstime=obstime, unit=unit
+            )
+            offset_applied.append(applied)
+
+        return tuple(offset_applied) if len(coords) > 1 else offset_applied[0]
 
 
 @dataclass
--- neclib/coordinates/paths/track.py
+++ neclib/coordinates/paths/track.py
@@ -29,23 +29,10 @@ class Track(Path):
         offset: Optional[Tuple[T, T, CoordFrameType]] = None,
         **ctx_kw: Any,
     ) -> None:
-        super().__init__(calc)
+        super().__init__(calc, *target, unit=unit)
 
         self._offset = offset
         self._ctx_kw = ctx_kw
-
-        if (len(target) == 1) and isinstance(target[0], str):
-            _target = target[0]
-        elif len(target) == 3:
-            lon, lat, frame = target
-            _target = self._calc.create_skycoord(lon, lat, frame=frame, unit=unit)
-        else:
-            raise TypeError(
-                "Invalid number of positional arguments: expected 1 "
-                "(target_name) or 3 (lon, lat, coordinate_frame), but got"
-                f" {len(target)}"
-            )
-        self._target = _target
 
         require_unit = (
             (offset is not None) and (not isinstance(offset[0], u.Quantity))
@@ -63,32 +50,13 @@ class Track(Path):
     @property
     def lonlat_func(self) -> Callable[[Index], Tuple[T, T]]:
         def _lonlat_func(idx: Index) -> Tuple[T, T]:
-            if isinstance(self._target, str):
-                _target = self._calc.get_body(self._target, idx.time)
-            else:
-                _target = self._target
-
-            if self._offset is not None:
-                offset_applied = self._calc.cartesian_offset_by(
-                    _target, *self._offset, unit=self._unit, obstime=idx.time
-                )
-                return offset_applied.data.lon, offset_applied.data.lat  # type: ignore
-            else:
-                return _target.data.lon, _target.data.lat  # type: ignore
+            target = self.get_skycoord(self._target, obstime=idx.time)
+            offset_applied = self.apply_offset(
+                target, offset=self._offset, obstime=idx.time, unit=self._unit
+            )
+            return offset_applied.data.lon, offset_applied.data.lat  # type: ignore
 
         return _lonlat_func
-
-    @property
-    def frame(self) -> CoordFrameType:
-        if isinstance(self._target, SkyCoord):
-            frame = self._target.frame
-        else:
-            frame = self._calc.get_body(self._target, time.time()).frame
-
-        if "obstime" in frame.frame_attributes:
-            frame = frame.replicate_without_data(obstime=None)
-
-        return frame
 
     @property
     def arguments(self) -> Tuple[Tuple[Any, ...], Dict[str, Any]]:
@@ -106,7 +74,7 @@ class Track(Path):
         if self._unit is not None:
             kwargs.update(unit=self._unit)
 
-        return (self.lonlat_func, self.frame), {
+        return (self.lonlat_func, self.target_frame), {
             "n_cmd": self.n_cmd,
             "context": ControlContext(**ctx_kw),
             "unit": self._unit,
