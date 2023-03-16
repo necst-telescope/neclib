0b5a32bdb71206f897b07771775425593c345fac
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2023-03-17 08:57:31 +0900

    #439 Reorganize object __module__

--- neclib/core/normalization/__init__.py
+++ neclib/core/normalization/__init__.py
@@ -1,7 +1,5 @@
 """Collection of type converters which allow type-flexible read-in."""
 
-from .array import NPArrayValidator
-from .function_defaults import partial
-from .quantity import QuantityValidator, get_quantity
-
-__all__ = ["get_quantity", "partial", "QuantityValidator", "NPArrayValidator"]
+from .array import NPArrayValidator  # noqa: F401
+from .astropy import QuantityValidator, get_quantity  # noqa: F401
+from .function_defaults import partial  # noqa: F401
