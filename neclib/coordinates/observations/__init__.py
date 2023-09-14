1a9b210b6f44847404583ded8cafe24755f0a066
Ryo <64296621+Kirby1717@users.noreply.github.com>
2023-09-14 15:42:36 +0900

    #482 Fix typo

--- neclib/coordinates/observations/__init__.py
+++ neclib/coordinates/observations/__init__.py
@@ -1,3 +1,3 @@
-from .optical_pointing import OptcalPointingSort  # noqa: F401
+from .optical_pointing import OpticalPointingSort  # noqa: F401  # Sort? Spec?
 from .otf import OTFSpec  # noqa: F401
 from .radio_pointing import RadioPointingSpec  # noqa: F401
