ae353621da07c7773d864f18670977623cd2605a
Ryo <64296621+Kirby1717@users.noreply.github.com>
2023-10-27 21:51:07 +0900

    #482 Add notes to m100.py

--- neclib/devices/ccd_controller/m100.py
+++ neclib/devices/ccd_controller/m100.py
@@ -5,6 +5,23 @@ from .ccd_controller_base import CCD_Controller
 
 
 class M100(CCD_Controller):
+
+    """ccd camera, which can capture stars when optical pointing.
+
+    Notes
+    -----
+    You need to install ``libgphoto2`` library in advance.
+
+    Configuration items for this device:
+
+    pic_captured_path : str
+        Save directory path of pictures captured by ccd camera.
+        e.g. "~/data/optical_pointing"
+
+    See defaults setting file in ``neclib/defaults/config.toml``.
+
+    """
+
     Model = "M100"
     Manufacturer = "Canon"
 
