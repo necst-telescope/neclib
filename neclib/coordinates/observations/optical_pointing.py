e819ac1b7f232430d6cc292584c25b1a26185c5d
Takuto Ito <takutoito@Takuto-no-MacBook-Pro.local>
2023-11-15 12:05:50 +0900

    #500 add function converting list pf pandas

--- neclib/coordinates/observations/optical_pointing.py
+++ neclib/coordinates/observations/optical_pointing.py
@@ -1,6 +1,8 @@
 from typing import List, Tuple, Union
 
 import numpy as np
+import pandas as pd
+
 from astropy import units as u
 from astropy.time import Time
 from matplotlib import pyplot as plt
@@ -21,8 +23,59 @@ class OpticalPointingSpec:
         return contents
 
     def _catalog_to_pandas(self, catalog_raw: List[str]):
-        # TODO: Implement.
-        ...
+        name_data = []
+        ra_data = []
+        dec_data = []
+        multiple_data = []
+        vmag_data = []
+        pmra_data = []
+        pmdec_data = []
+        az_data = []
+        el_data = []
+        for line in catalog_raw:
+            try:
+                name = line[7:14]
+                ra = (
+                    float(line[75:77]) * u.hourangle
+                    + float(line[77:79]) * u.arcmin
+                    + float(line[79:83]) * u.arcsec
+                ).to(u.deg)
+                dec = (
+                    float(line[84:86]) * u.deg
+                    + float(line[86:88]) * u.arcmin
+                    + float(line[88:90]) * u.arcsec
+                ).to(u.deg)
+                if line[83:84] == "-":
+                    dec = -dec
+                altaz = self.to_altaz(target=(ra, dec), frame="fk5")
+                multiple = line[43:44]
+                vmag = float(line[103:107])
+                pmra = float(line[149:154])
+                pmdec = float(line[154:160])
+                ra_data.append(ra)
+                dec_data.apend(dec)
+                multiple_data.append(multiple)
+                vmag_data.append(vmag)
+                pmra_data.append(pmra)
+                pmdec_data.append(pmdec)
+                name_data.append(name)
+                az_data.append(altaz.az)
+                el_data.append(altaz.alt)
+            except Exception:
+                pass
+        data = pd.DataFrame(
+            {
+                "name": name_data,
+                "ra": ra_data,
+                "dec": dec_data,
+                "pmra": pmra_data,
+                "pmdec": pmdec_data,
+                "az": az_data,
+                "el": el_data,
+                "vmag": vmag_data,
+            }
+        )
+        return data
 
     def to_altaz(self, target: Tuple[u.Quantity, u.Quantity], frame: str, time=0.0):
         # if time == 0.0:
@@ -39,65 +92,37 @@ class OpticalPointingSpec:
         altaz_coord = coord.to_apparent_altaz()
         return altaz_coord
 
-    def _filter(self, catalog: List[str], magnitude: float) -> List[str]:
-        # TODO: Implement.
-        ...
+    def _filter(
+        self, catalog: pd.DataFrame, magnitude: Tuple[float, float]
+    ) -> pd.DataFrame:
+        az_range = config.antenna_drive_warning_limit_az
+        el_range = config.antenna_drive_warning_limit_el
+        filtered = catalog[
+            (catalog["az"] in az_range)
+            & (catalog["el"] in el_range)
+            & (catalog["multiple"] == "")
+            & (catalog["pmra"] <= 1.0)
+            & (catalog["pmdec"] <= 1.0)
+            & (catalog["vmag"] >= magnitude[0])
+            & (catalog["vmag"] <= magnitude[1])
+        ]
+        return filtered
 
     def sort(
         self, catalog_file: str, magnitude: Tuple[float, float]
     ):  # HACK: Now almost copied from v3.
-        # from operator import itemgetter
-        def itemgetter(item):
-            return lambda x: x[item]
+        az_range = config.antenna_drive_warning_limit_az
 
         catalog_raw = self.readlines_file(filename=catalog_file)
-        # catalog = self._catalog_to_pandas(catalog_raw=catalog_raw)
-        az_range = config.antenna_drive_warning_limit_az
-        el_range = config.antenna_drive_warning_limit_el
-        data = []
-        for line in catalog_raw:
-            try:
-                ra2000 = (
-                    float(line[75:77])
-                    + float(line[77:79]) / 60.0  # arcmin
-                    + float(line[79:83]) / 3600.0  # arcsec
-                ) * u.hourangle
-                ra2000 = ra2000.to(u.deg)
-                dec2000 = (
-                    float(line[84:86]) * u.deg
-                    + float(line[86:88]) * u.arcmin
-                    + float(line[88:90]) * u.arcsec
-                )
-                if line[83:84] == "-":
-                    dec2000 = -dec2000
-                multiple = line[43:44]
-                vmag = float(line[103:107])
-                pmra = float(line[149:154])
-                pmdec = float(line[154:160])
-                altaz = self.to_altaz(target=(ra2000, dec2000), frame="fk5")
-                # print(f"az: {altaz.az}, el: {altaz.alt}")
+        catalog = self._catalog_to_pandas(catalog_raw=catalog_raw)
+        catalog = self._filter(catalog, magnitude)
 
-                if (
-                    altaz.az in az_range
-                    and altaz.alt in el_range
-                    and multiple == " "
-                    and vmag >= magnitude[0]
-                    and vmag <= magnitude[1]
-                    and pmra <= 1.0  # parameterized from v3
-                    and pmdec <= 1.0  # parameterized from v3
-                ):
-                    data.append(
-                        [line[7:14], ra2000, dec2000, pmra, pmdec, altaz.az, altaz.alt]
-                    )
-            except Exception:
-                pass
-
-        sdata = np.array(sorted(data, key=itemgetter(5)))  # sort by az
+        sdata = catalog.sort_values("az")  # sort by az
         # print(f"sdata: {sdata}")
-        tmp = sdata[:, 5].astype(np.float64)
-
+        # tmp = sdata["az"].astype(np.float64)
         # print("sdata", tmp)
-        ddata = np.array([]).reshape(0, 7)
+        # ddata = np.array([]).reshape(0, 7)
+        ddata = pd.DataFrame()
         elflag = 0
         azint = 100 * u.deg
         # print(f"az_range[0]: {az_range.lower.value}")
@@ -106,20 +131,27 @@ class OpticalPointingSpec:
             # print("azmin, azmax, azint:", az_range[0], az_range[1], azint)
             # print(f"min: {min(azaz, azaz + azint.value)}")
             # print(f"max: {max(azaz, azaz + azint.value)}")
-            ind = np.where(
-                (tmp > min(azaz, azaz + azint.value))
-                & (tmp < max(azaz, azaz + azint.value))
-            )
+            # ind = np.where(
+            #    (tmp > min(azaz, azaz + azint.value))
+            #    & (tmp < max(azaz, azaz + azint.value))
+            # )
+            ind = sdata[
+                (sdata["az"] >= min(azaz, azaz + azint.value))
+                & (sdata["az"] <= max(azaz, azaz + azint.value))
+            ]
+
             # print("ind", ind)
             # print("len ind", len(ind))
-            dum = sdata[ind[0], :]
-            ind2 = np.argsort(dum[:, 6])
+
+            # dum = sdata[ind[0], :]
+            # ind2 = np.argsort(dum[:, 6])
+            ind2 = ind.sort_values("el")
             if elflag == 0:
                 elflag = 1
             else:
                 ind2 = ind2[::-1]
                 elflag = 0
-            ddata = np.append(ddata, dum[ind2, :], axis=0)
+            ddata.join(ind2)
             continue
 
         # x = np.round(ddata[:, 5].astype(np.float64), 2)
@@ -128,8 +160,10 @@ class OpticalPointingSpec:
         # y = ddata[:, 6]  # .astype(np.float64)
         # print(f"x: {x}")
         # print(f"y: {y}")
-        x = ddata[:, 5].astype(np.float64)
-        y = ddata[:, 6].astype(np.float64)
+        # x = ddata[:, 5].astype(np.float64)
+        # y = ddata[:, 6].astype(np.float64)
+        x = ddata["az"].astype(np.float64)
+        y = ddata["el"].astype(np.float64)
         # print(f"x_astype: {x}")
         # print(f"y_astype: {y}")
         show_graph = True
