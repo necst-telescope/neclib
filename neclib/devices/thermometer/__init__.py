dc89dbb1822001d2153b14ca9f7051370d7e3554
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2024-04-01 13:33:27 +0900

    #518 change config setting

--- neclib/devices/thermometer/__init__.py
+++ neclib/devices/thermometer/__init__.py
@@ -1,3 +1,2 @@
 from .model_218 import Model218  # noqa:F401
-from .model_218_usb import Model218USB  # noqa:F401
 from .simulator import ThermometerSimulator  # noqa:F401
--- neclib/devices/thermometer/model_218.py
+++ neclib/devices/thermometer/model_218.py
@@ -3,6 +3,7 @@ import time
 import astropy.units as u
 import ogameasure
 
+from ... import get_logger
 from ...core.security import busy
 from .thermometer_base import Thermometer
 
@@ -10,28 +11,53 @@ from .thermometer_base import Thermometer
 class Model218(Thermometer):
     """Thermometer, which can check tempareture of cryostat.
 
-    Notes
-    -----
-    If you use this device with GPIB, you should write "Model218" in `config.toml`
-
     Configuration items for this device:
+
+    communicator : str
+        Communicator of thermometer. GPIB or USB can be chosen.
 
     host : str
         IP address for GPIB communicator.
+        If you use GPIB communicator, you must set this parameter.
 
     port : int
         GPIB port of using devices. Please check device setting.
+        If you use GPIB communicator, you must set this parameter.
+
+    usb_port : str
+        USB port of using devices.
+        If you use USB communicator, you must set this parameter.
+
+    channel : Dict[str, int]
+        Human-readable channel name. The value should be mapping from human readable
+        version (str) to device level identifier (int). You can assign any name to the
+        channels.
 
     """
 
     Manufacturer = "LakeShore"
     Model = "Model218"
 
-    Identifier = "host"
+    if Thermometer.Config.communicator == "GPIB":
+        Identifier = "host"
+    else:
+        Indentifier = "usb_port"
 
     def __init__(self) -> None:
-        com = ogameasure.gpib_prologix(host=self.Config.host, gpibport=self.Config.port)
-        self.thermometer = ogameasure.Lakeshore.model218(com)
+        self.logger = get_logger(self.__class__.__name__)
+
+        if self.Config.communicator == "GPIB":
+            com = ogameasure.gpib_prologix(
+                host=self.Config.host, gpibport=self.Config.port
+            )
+            self.thermometer = ogameasure.Lakeshore.model218(com)
+        elif self.Config.communicator == "USB":
+            self.thermometer = ogameasure.Lakeshore.model218_usb(self.Config.usb_port)
+        else:
+            self.logger.warning(
+                f"There is not exsited communicator: {self.Config.communicator}."
+                "Please choose USB or GPIB."
+            )
 
     def get_temp(self, id: str) -> u.Quantity:
         ch = self.Config.channel[id]
