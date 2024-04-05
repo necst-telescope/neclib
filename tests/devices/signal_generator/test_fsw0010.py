6c8eae785b3392ffc6540c36ec2de08fe16ffa3c
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2024-04-05 14:59:23 +0900

    #523 add attenuator

--- tests/devices/signal_generator/test_fsw0010.py
+++ tests/devices/signal_generator/test_fsw0010.py
@@ -5,7 +5,7 @@ from neclib.devices.signal_generator.fsw0010 import FSW0010
 from ..conftest import get_instance
 
 pytestmark = pytest.mark.skipif(
-    get_instance(FSW0010) is None, reason="CPZ7415V is not configured"
+    get_instance(FSW0010) is None, reason="FSW0010 is not configured"
 )
 
 
