a87845aa41eaebfd08ba00752d3eb18ab6efbb2b
takayama-27 <t.fuuna@a.phys.nagoya-u.ac.jp>
2024-04-10 16:31:53 +0900

    #528 使う関数を変えた

--- neclib/devices/attenuator/cpz340516.py
+++ neclib/devices/attenuator/cpz340516.py
@@ -13,22 +13,19 @@ class CPZ340516(Attenuator):
     -----
     Configuration items for this device:
 
-    rate :
+    rate : {0.1}
 
-    rsw_id : {0, 1, ..., 16} or {"0", "1", ..., "9", "A", ..., "F"}
+    rsw_id : {0, 1, ..., 7}
         Board identifier. This should be set to the same value as the rotary switch
         "RSW1" mounted on the side of the board. The board is shipped with default RSW1
         setting of 0. This ID would be non-zero, when multiple PCI board of same model
         are mounted on a single FA (Factory Automation) controller.
 
-    channnel_number : int
-
     See defaults setting file in ``neclib/defaults/config.toml``.
 
     """
 
-    # Manufacturerとは? Identifierもこれでいいのか要確認
-    Manufacturer = ""
+    Manufacturer = "Interface"
     Model = "CPZ340516"
 
     Identifier = "rsw_id"
@@ -36,25 +33,20 @@ class CPZ340516(Attenuator):
     def __init__(self):
         import pyinterface
 
-        self.rate = self.Config.rate
         self.rsw_id = self.Config.rsw_id
-        self.param_buff = {i: 0.0 for i in range(1, 17)}
-
         self.io = pyinterface.open(3405, self.rsw_id)
-        # pyinterface/toolsで340816と書き方が違うけど多分？
 
-    def get_loss(self, ch: int) -> u.Quantity:
+    def get_loss(self, ch: int, current: float) -> u.Quantity:
+        with busy(self, "busy"):
+            return self.io.output_current(ch, current) << u.mA
+
+    def set_loss(self, ch: int, outputrange: str) -> None:
         with busy(self, "busy"):
             try:
-                return self.io.get_outputrange(ch) << u.mA
+                self.io.set_outputrange(ch, outputrange)
             except IndexError:
                 pass
             raise ValueError(f"Invalid channel: {ch}")
 
-    def set_loss(self, mA: float, id: str, outputrange: str) -> None:
-        with busy(self, "busy"):
-            ch = self.Config.channel[id]
-            self.io.set_outputrange(ch, outputrange)
-
     def finalize(self) -> None:
-        self.io.com.close()
+        self.io.finalize()
