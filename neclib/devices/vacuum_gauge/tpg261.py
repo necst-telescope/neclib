5614496f7705689f42132320aa6123b61390ed15
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2024-05-10 17:21:23 +0900

    #536 add communicator

--- neclib/devices/vacuum_gauge/tpg261.py
+++ neclib/devices/vacuum_gauge/tpg261.py
@@ -1,6 +1,7 @@
 import astropy.units as u
 import ogameasure
 
+from ... import get_logger
 from ...core.security import busy
 from .vacuum_gauge_base import VacuumGauge
 
@@ -24,16 +25,33 @@ class TPG261(VacuumGauge):
     Model = "TPG261"
     Manufacturer = "Pfeiffer"
 
-    Identifier = "host"
+    Identifier = "communicator"
 
     def __init__(self):
-        com = ogameasure.ethernet(self.Config.host, self.Config.port)
-        self.io = ogameasure.Pfeiffer.tpg261_lan(com)
-        self.io.pres_unit_torr()
+        self.logger = get_logger(self.__class__.__name__)
+
+        if self.Config.communicator == "LAN":
+            com = ogameasure.ethernet(self.Config.host, self.Config.port)
+            self.io = ogameasure.Pfeiffer.tpg261_lan(com)
+            self.io.pres_unit_torr()
+
+        elif self.Config.communicator == "RS232":
+            com = ogameasure.serial(self.Config.host, self.Config.port)
+            self.io = ogameasure.Pfeiffer.tpg261(com)
+            self.io.unit_command(unit=1)
+
+        else:
+            self.logger.warning(
+                f"There is not exsited communicator: {self.Config.communicator}."
+                "Please choose RS232 or LAN."
+            )
 
     def get_pressure(self) -> u.Quantity:
         with busy(self, "busy"):
-            return self.io.pressure() * u.torr
+            if self.Config.communicator == "LAN":
+                return self.io.pressure() * u.torr
+            elif self.Config.communicator == "RS232C":
+                return self.io.read_pressure() * u.torr
 
     def finalize(self):
         self.io.com.close()
