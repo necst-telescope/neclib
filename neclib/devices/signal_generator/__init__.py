f410298fa6a0763727f9247f0ebb221759f741f7
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2024-05-13 00:58:02 +0900

    #534 add Anritsu SG

--- neclib/devices/signal_generator/__init__.py
+++ neclib/devices/signal_generator/__init__.py
@@ -1,3 +1,4 @@
 from .fsw0010 import FSW0010  # noqa: F401
 from .fsw0020 import FSW0020  # noqa: F401
+from .mg3692c import MG3692C  # noqa: F401
 from .simulator import SignalGeneratorSimulator  # noqa: F401
