c855f40e724e43aeb4b2b53b62100a7fd3948d82
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-08-11 14:28:41 +0900

    #621 debug

--- neclib/controllers/antenna_pid.py
+++ neclib/controllers/antenna_pid.py
@@ -220,7 +220,7 @@ class PIDController:
         if np.isnan(self.cmd_speed[Now]):
             for i in range(2):
                 self.cmd_speed.push(0)
-        for i in range(10):
+        for i in range(50):
             self.cmd_time.push(now)
             self.cmd_coord.push(cmd_coord)
             self.target_speed.push(0)
@@ -233,12 +233,12 @@ class PIDController:
         """Define control loop parameters."""
         if not hasattr(self, "cmd_speed"):
             self.cmd_speed = ParameterList.new(2)
-        self.cmd_time = ParameterList.new(10)
+        self.cmd_time = ParameterList.new(50)
         self.enc_time = ParameterList.new(2 * int(self.error_integ_count / 2))
-        self.cmd_coord = ParameterList.new(10)
+        self.cmd_coord = ParameterList.new(50)
         self.enc_coord = ParameterList.new(2 * int(self.error_integ_count / 2))
         self.error = ParameterList.new(2 * int(self.error_integ_count / 2))
-        self.target_speed = ParameterList.new(10)
+        self.target_speed = ParameterList.new(50)
 
     def get_speed(
         self,
