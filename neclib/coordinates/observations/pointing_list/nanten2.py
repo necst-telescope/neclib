bf1adfe0355e5e34c3461822054aa91cf994113d
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-08-21 18:02:38 +0900

    #649 fix

--- neclib/coordinates/observations/pointing_list/nanten2.py
+++ neclib/coordinates/observations/pointing_list/nanten2.py
@@ -1,3 +1,11 @@
+from typing import Tuple
+
+import pandas as pd
+
+from astropy.coordinates import Angle
+from astropy import units as u
+
+from ....core import config
 from .pointing_list import PointingList
 
 
