bc5d75e9ac463dffe4afadd870152cc4d6089d6a
Yuito Okamoto <sfc05023@st.osakafu-u.ac.jp>
2024-11-15 13:39:49 +0900

    #674 add calc_tp

--- neclib/devices/spectrometer/spectrometer_base.py
+++ neclib/devices/spectrometer/spectrometer_base.py
@@ -3,9 +3,14 @@ from typing import Dict, List, Tuple
 
 from ..device_base import DeviceBase
 
+import numpy as np
+
 
 class Spectrometer(DeviceBase):
     @abstractmethod
     def get_spectra(self) -> Tuple[float, Dict[int, List[float]]]:
         """Timestamp and dict of spectral data for all boards."""
         ...
+    
+    def calc_tp(data: Dict[int, List[float]]) -> float:
+        return np.nansum(data[1])
