24463dec750806c8301cca06399ec8289933633a
Yuito Okamoto <sfc05023@st.osakafu-u.ac.jp>
2024-12-27 16:40:04 +0900

    #674 tp to list and stored in tp_dict

--- neclib/devices/spectrometer/spectrometer_base.py
+++ neclib/devices/spectrometer/spectrometer_base.py
@@ -12,10 +12,10 @@ class Spectrometer(DeviceBase):
         """Timestamp and dict of spectral data for all boards."""
         ...
 
-    def calc_tp(self, data: Dict[int, List[float]]) -> dict:
+    def calc_tp(self, data: Dict[int, Tuple[float]]) -> dict:
         tp_dict = {}
         for board_id, spectral_data in data.items():
             tp = np.nansum(spectral_data)
-            tp_dict[board_id] = tp
-            pass
+            tp_list = [np.float32(tp)]
+            tp_dict[board_id] = tp_list
         return tp_dict
