e297eb4fb4091bbd0e27ba3a06d8ee38c840a944
masahirokato0116 <masakato@a.phys.nagoya-u.ac.jp>
2025-07-01 08:41:49 +0900

    make ondotori tr72w

---
+++ neclib/devices/weather_station/tr72w.py
@@ -0,0 +1,61 @@
+__all__ = ["TR72W"]
+
+import struct
+import time
+from typing import Dict
+import urllib.request
+
+import astropy.units as u
+import ogameasure
+
+from ... import get_logger
+from ...core.security import busy
+from .weather_station_base import WeatherStation
+
+IP = "192.168.100.105"
+URL = 'http://'+IP+'/B/crrntdata/cdata.txt'
+
+class TR72W(WeatherStation):
+
+    def __init__(self) -> None:
+
+    def _get_data(self) -> Dict[str,float]:
+        with busy(self,"busy"):
+            try:
+                res = urllib.request.urlopen(URL)
+                page = res.read()
+                decoded_page = page.decode('shift_jis')
+                raw_data = decoded_page.split('\r\n')
+                time.sleep(30)
+                raw_T1 = raw_data[5].split('=')
+                raw_T2 = raw_data[6].split('=')
+                if raw_T1[1] != '----':
+                    temp1 = float(raw_T1[1])
+                else:
+                    temp1 = 0
+                if raw_T2[1] != '----':
+                    temp2 = float(raw_T2[1])
+                else:
+                    temp2 = 0
+                return {"temp": temp1,"humid": temp2}
+            except struct.error:
+                self.logger.warning("Failed to get data from TR73U")
+                return {"temp": -273.15, "humid": 0.0}
+
+    def get_temperature(self) -> u.Quantity:
+        data = self._get_data()
+        data_K = (data["temp"] * u.deg_C).to(u.K, equivalencies=u.temperature())
+        return data_K
+
+    def get_humidity(self) -> float:
+        data = self._get_data()
+        return data["humid"] * 0.01
+
+    def get_pressure(self) -> u.Quantity:
+        return 0
+
+    def finalize(self) -> None:
+        return
+
+    def close(self) -> None:
+        return
