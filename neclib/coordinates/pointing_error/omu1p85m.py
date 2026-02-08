461861ceaed5164b1f15ceeb1df311eae65181de
Fumika Demachi <f.demachi@a.phys.nagoya-u.ac.jp>
2026-02-08 16:24:11 +0900

    #732 return daz del in inverse_offset

--- neclib/coordinates/pointing_error/omu1p85m.py
+++ neclib/coordinates/pointing_error/omu1p85m.py
@@ -134,7 +134,7 @@ class OMU1P85M(PointingError):
             )
             return f
 
-        az0, el0 = self.apply_offset(az, el)
+        az0, el0, _dAz, _dEl = self.apply_offset(az, el)
         x0 = np.array([az0.deg, el0.deg])
 
         ans = scipy.optimize.root(res, x0, method="hybr", tol=1e-13)
