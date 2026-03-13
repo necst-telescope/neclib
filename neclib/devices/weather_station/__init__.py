0c686b3cacf9da8958974c1479ee9286cbbc7f55
Fumika Demachi <f.demachi@a.phys.nagoya-u.ac.jp>
2026-03-13 13:12:57 -0300

    Revert "#756 add davis in init"
    This reverts commit c169f629d936a96bd058e4f761003321ad7b3918.

--- neclib/devices/weather_station/__init__.py
+++ neclib/devices/weather_station/__init__.py
@@ -1,4 +1,3 @@
-from .davis import Davis  # noqa: F401
 from .simulator import WeatherStationSimulator  # noqa: F401
-from .tr72w import TR72W  # noqa: F401
 from .tr73u import TR73U  # noqa: F401
+from .tr72w import TR72W  # noqa: F401
